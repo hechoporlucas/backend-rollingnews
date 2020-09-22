5d7e480289f4397f9a80dcf1da90eca72e25213f
Lucas <lucasmart.tuc@gmail.com>
2020-09-22 12:27:17 -0300

    se creo la estructura basica del servidor

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path, { dirname } from "path";
+
+const app = express(); //instancia de express
+
+app.set("port", process.env.PORT || 4000); //creamos una variable para el puerto
+
+app.listen(app.get("port"), () => {
+  console.log(path.join(__dirname, "../public"));
+  console.log("estamos escuchando el puerto " + app.get("port"));
+});
+
+//middlewares...
+app.use(morgan("dev"));
+app.use(cors()); //para recibir solicitudes remotas (get, put, post, etc).
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //para entender json tmb el de arriba.
+app.use(express.static(path.join(__dirname, "../public"))); //es para decir q nuestra carpeta public es estatica (y para acceder tmb);
+
+//Ruta...
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+
+app.get("/contacto", (req, res) => {
+  res.send("estoy en la pagina contacto");
+});
