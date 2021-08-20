8452182c6084f770887248fb71938382ef8c415e
Juan Arias <arias.juan.jose@outlook.com>
2021-08-20 12:27:53 -0300

    mongo v5

--- src/database.js
+++ src/database.js
@@ -1,21 +1,12 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/api";
+const url = "mongodb+srv://JuanArias:juan@cluster0.pzjkj.mongodb.net/api?retryWrites=true&w=majority";
 
 mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
     useUnifiedTopology: true
-});
-
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://JuanArias:juan@cluster0.pzjkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
 });
 
 
