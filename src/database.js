f36d2a8c0fea9aef44f86aecf0a92485e7fa02ff
Juan Arias <arias.juan.jose@outlook.com>
2021-08-20 12:23:12 -0300

    mongo v4

--- src/database.js
+++ src/database.js
@@ -9,6 +9,16 @@ mongoose.connect(url, {
     useUnifiedTopology: true
 });
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://JuanArias:juan@cluster0.pzjkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 const connection = mongoose.connection
 
 connection.once('open', () => {
