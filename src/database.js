c44beb978a80c6e711c76a7a21ec85a627b022f5
Juan Arias <arias.juan.jose@outlook.com>
2021-08-20 19:20:34 -0300

    volver

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://JuanArias:juan@cluster0-shard-00-00.pzjkj.mongodb.net:27017,cluster0-shard-00-01.pzjkj.mongodb.net:27017,cluster0-shard-00-02.pzjkj.mongodb.net:27017/api?ssl=true&replicaSet=atlas-12uw9l-shard-0&authSource=admin&retryWrites=true&w=majority";
+const url = "mongodb://localhost:27017/test";
 
 mongoose.connect(url, {
     useNewUrlParser: true,
@@ -8,7 +8,6 @@ mongoose.connect(url, {
     useFindAndModify: true,
     useUnifiedTopology: true
 });
-
 
 const connection = mongoose.connection
 
